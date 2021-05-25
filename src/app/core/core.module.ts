ecb24c6fe2de6f52c98808111a752f73ac088d28
christophe <christophegueroult@gmail.com>
2021-05-25 14:29:37 +0200

    etape 2 : import, exports CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { UiModule } from 'projects/ui/src/public-api';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [UiModule, IconsModule, TemplatesModule],
 })
-export class CoreModule { }
+export class CoreModule {}

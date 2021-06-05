c5c26e066831935ef56e6fb2ca76089ca7d29180
christophe <christophegueroult@gmail.com>
2021-06-05 15:58:44 +0200

    Etape 8, attribute directive ex and event binding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent implements OnInit {
+  /**
+   * open a true pour ouvrir le menu par defaut
+   */
+  public open = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public toggle() {
+    this.open = !this.open;
   }
-
 }
